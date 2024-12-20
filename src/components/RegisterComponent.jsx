3b599b80a89d51b8628ad4adeaa411563b5b14cf
KyokaKyoka <752572249@qq.com>
2024-12-20 04:11:13 +0000

    update register url

--- src/components/RegisterComponent.jsx
+++ src/components/RegisterComponent.jsx
@@ -30,8 +30,8 @@ function RegisterComponent() {
     }
 
     try {
-      const response = await axios.post('http://localhost:8080/register', {
-      // const response = await axios.post('http://3.252.231.197/register', {
+      // const response = await axios.post('http://localhost:8080/register', {
+      const response = await axios.post('http://3.252.231.197/register', {
         username,
         password,
         roles
